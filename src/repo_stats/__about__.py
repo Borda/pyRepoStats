d5f7325fee049ab6eb6a6e53c5f64c18d054ef8d
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2023-11-18 16:33:25 +0100

    Apply suggestions from code review
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- src/repo_stats/__about__.py
+++ src/repo_stats/__about__.py
@@ -3,10 +3,10 @@ __author__ = "Jiri Borovec"
 __author_email__ = "j.borovec@...com"
 __license__ = "MIT"
 __homepage__ = "https://borda.github.io/pyRepoStats"
-__copyright__ = "Copyright (c) 2020-2023, %s." % __author__
+__copyright__ = f"Copyright (c) 2020-2023, {__author__}."
 __docs__ = "Repository Statistics"
 __long_doc__ = (
-    "# %s" % __docs__
+    f"# {__docs__}"
     + """
 
 This simple tool aims on open-source projects providing simple repository stats
